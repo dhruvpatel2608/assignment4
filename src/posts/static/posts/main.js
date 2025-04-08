cd108faaf9d51dc9099c298fc124750e653637ec
dhruvpatel2608 <dpatel0044@conestogac.on.ca>
2025-04-08 00:41:00 -0400

    I am done with getting posts objects with ajax and adding spinner and posts cards

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,6 +1,8 @@
 console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 
 $.ajax({
@@ -12,5 +14,41 @@ $.ajax({
     },
     error: function(error){
         console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                    <div class="card mb-4" >
+                        <div class="card-body">
+                            <h5 class="card-title">${el.title}</h5>
+                            <p class="card-text">${el.body}</p>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-1">
+                                    <a href="#" class="btn btn-primary">Like</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>                
+                `
+            });
+        }, 100)
+    },
+    error: function(error){
+        console.log(error)
     }
 })
